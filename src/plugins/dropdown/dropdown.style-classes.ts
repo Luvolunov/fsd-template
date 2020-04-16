241a466f3d63a87d143dba81afe37afb69058193
Vladimir <luvolunov@gmail.com>
2020-04-16 23:02:49 +0900

    added: styles for dropdown

--- src/plugins/dropdown/dropdown.style-classes.ts
+++ src/plugins/dropdown/dropdown.style-classes.ts
@@ -1,7 +1,13 @@
 export const DROPDOWN_STYLE_CLASSES: {[key: string]: string} = {
-    input: "",
-    outerRows: "",
-    row: "",
-    btn: "",
-    text: ""
+    dropdown: "dropdown",
+    input: "dropdown__input",
+    outerRows: "dropdown__outer-rows",
+    row: "dropdown__row",
+    lefthand: "dropdown__lefthand",
+    righthand: "dropdown__righthand",
+    btn: "dropdown__btn",
+    text: "dropdown__text",
+    outerActions: "dropdown__outer-actions",
+    clear: "dropdown__clear",
+    apply: "dropdown__apply"
 }
