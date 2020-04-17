b90749ed769ac4c1477832e011f918c1edc0d65f
Vladimir <luvolunov@gmail.com>
2020-04-17 10:06:54 +0900

    refactored: counters

--- src/plugins/dropdown/dropdown-options.model.ts
+++ src/plugins/dropdown/dropdown-options.model.ts
@@ -1,9 +1,11 @@
+import { Counter } from "./counter.model";
+
 export interface DropdownOptionsModel{
     // Will actions be in dropdown (clear, accept)
     withActions?: boolean;
 
     // counters object as state
-    counters?: {[key: string]: {name: string, value: number}};
+    counters?: {[key: string]: Counter};
 
     // initial string in input
     viewText?: string;
