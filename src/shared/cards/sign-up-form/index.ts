48bedd11ef064d239b50c89ef1561c5c368af43d
Vladimir <luvolunov@gmail.com>
2020-04-20 15:36:44 +0900

    added: sign up form

---
+++ src/shared/cards/sign-up-form/index.ts
@@ -0,0 +1,6 @@
+import "./index.scss";
+import "../../components/standart-input/index";
+import "../../components/standart-radio/index";
+import "../../components/toggle-button/index";
+import "../../components/button_arrow/index";
+import "../../components/button_bg_white/index";
