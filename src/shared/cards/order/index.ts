aee5d54bdcc351a1131e6db744be6be81f4c03e8
Vladimir <luvolunov@gmail.com>
2020-04-22 17:40:36 +0900

    added: calendars to components

--- src/shared/cards/order/index.ts
+++ src/shared/cards/order/index.ts
@@ -2,6 +2,7 @@ import "./index.scss";
 import "../../components/small-filter-date/index";
 import "../../components/button_arrow/index";
 import { DropdownController } from "../../../plugins/dropdown/dropdown.controller";
+import { CalendarController } from "../../../plugins/calendar/calendar.controller";
 
 const dropdowns = Array.from(document.querySelectorAll(".js-order .js-dropdown"))
                        .map(
@@ -12,3 +13,25 @@ const dropdowns = Array.from(document.querySelectorAll(".js-order .js-dropdown")
                                                            }
                                                          )
                        )
+
+const forms = document.querySelectorAll(".js-order");
+
+const calendars = Array.from(document.querySelectorAll(".js-order .js-calendar"))
+                      .map((el, index) => {
+                            const c = new CalendarController(el as HTMLDivElement)
+                            forms[index].addEventListener("click", (e) => {
+                                const el = e.target as HTMLElement;
+                                if (el.hasAttribute("data-calendar-toggle")){
+                                    c.toggle();
+                                }
+                            })
+                            c.onApply((first: Date, second: Date) => {
+                                const f = forms[index].querySelector(".js-calendar-from") as HTMLInputElement;
+                                const t = forms[index].querySelector(".js-calendar-to") as HTMLInputElement;
+
+                                f.value = first.toLocaleDateString();
+                                t.value = second.toLocaleDateString();
+                                c.toggle();
+                            })
+                            return c;
+                      })
--- src/shared/cards/search-form/index.ts
+++ src/shared/cards/search-form/index.ts
@@ -2,6 +2,7 @@ import "./index.scss";
 import "../../components/small-filter-date/index";
 import "../../components/button_arrow/index";
 import { DropdownController } from "../../../plugins/dropdown/dropdown.controller";
+import { CalendarController } from "../../../plugins/calendar/calendar.controller";
 
 const dropdowns = Array.from(document.querySelectorAll(".js-search-form .js-dropdown"))
                        .map((el) => new DropdownController(el as HTMLDivElement, 
@@ -23,4 +24,26 @@ const dropdowns = Array.from(document.querySelectorAll(".js-search-form .js-drop
                                                 }
                                             }
                                         }
-                       ))
+                       ));
+
+const forms = document.querySelectorAll(".js-search-form");
+
+const calendars = Array.from(document.querySelectorAll(".js-search-form .js-calendar"))
+                       .map((el, index) => {
+                            const c = new CalendarController(el as HTMLDivElement)
+                            forms[index].addEventListener("click", (e) => {
+                                const el = e.target as HTMLElement;
+                                if (el.hasAttribute("data-calendar-toggle")){
+                                    c.toggle();
+                                }
+                            })
+                            c.onApply((first: Date, second: Date) => {
+                                const f = forms[index].querySelector(".js-calendar-from") as HTMLInputElement;
+                                const t = forms[index].querySelector(".js-calendar-to") as HTMLInputElement;
+
+                                f.value = first.toLocaleDateString();
+                                t.value = second.toLocaleDateString();
+                                c.toggle();
+                            })
+                            return c;
+                       })
